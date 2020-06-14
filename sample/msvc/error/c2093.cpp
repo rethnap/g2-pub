8a26f7efc9c7e5409cfe9458fb42013fa92a9be4
pinpan <pinpan@localhost>
2020-06-14 20:46:41 +0900

    #0000 misc

---
+++ sample/msvc/error/c2093.cpp
@@ -0,0 +1,10 @@
+// C2093.c
+// compile with: /Za /c
+void func() {
+   int li;   // an implicit auto variable
+   int * s[]= { &li };   // C2093 address is not a constant
+
+   // OK
+   static int li2;
+   int * s2[]= { &li2 };
+}
